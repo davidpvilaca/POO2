1dcafaaf721d6352df4ca1694b7dbef414c12ebf
David Vilaça <davidpvilaca@outlook.com>
2017-03-12 17:01:07 -0300

    Adicionado exercicio 9 - Tema Interface Fabrica

---
+++ Exercicio9-TemaInterfaceFabrica/src/fabrica/abstrata/botoes/FabricaPadrao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabrica.abstrata.botoes;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author davidpvilaca
+ */
+public class FabricaPadrao extends FabricaAbstrataBotoes {
+
+    @Override
+    public JButton criaBotaoOK() {
+        return new JButton();
+    }
+
+    @Override
+    public JButton criaBotaoCancel() {
+        return new JButton();
+    }
+    
+}
